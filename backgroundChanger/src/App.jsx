f7532491d2791060246f65451da105ef4165f7d7
samarth goyal <samarthagrawal1804@gmail.com>
2023-08-29 02:14:12 +0530

    Complete all logics of project

--- backgroundChanger/src/App.jsx
+++ backgroundChanger/src/App.jsx
@@ -1,15 +1,96 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  
+  const [color, setColor] = useState("white");
 
   return (
-    <>
-     <h1>Hello Developers</h1>
-      
-    </>
-  )
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-3 ">
+        <div className="flex flex-wrap justify-center gap-3 px-3 py-2 shadow-lg bg-gray-300 rounded-2xl">
+          <button
+            className="btn-primary"
+            style={{ backgroundColor: "red" }}
+            onClick={() => setColor("red")}
+          >
+            Red
+          </button>
+          <button
+            className="btn-primary"
+            style={{ backgroundColor: "Green" }}
+            onClick={() => setColor("green")}
+          >
+            Green
+          </button>
+          <button
+            className="btn-primary"
+            style={{ backgroundColor: "Blue" }}
+            onClick={() => setColor("blue")}
+          >
+            Blue
+          </button>
+          <button
+            className="btn-primary"
+            style={{ backgroundColor: "Olive" }}
+            onClick={() => setColor("olive")}
+          >
+            Olive
+          </button>
+          <button
+            className="btn-primary text-black"
+            style={{ backgroundColor: "white" }}
+            onClick={() => setColor("white")}
+          >
+            White
+          </button>
+          <button
+            className="btn-primary"
+            style={{ backgroundColor: "Gray" }}
+            onClick={() => setColor("gray")}
+          >
+            Gray
+          </button>
+          <button
+            className="btn-primary text-black"
+            style={{ backgroundColor: "Yellow" }}
+            onClick={() => setColor("yellow")}
+          >
+            Yellow
+          </button>
+          <button
+            className="btn-primary text-black"
+            style={{ backgroundColor: "Pink" }}
+            onClick={() => setColor("pink")}
+          >
+            Pink
+          </button>
+          <button
+            className="btn-primary"
+            style={{ backgroundColor: "Purple" }}
+            onClick={() => setColor("purple")}
+          >
+            Purple
+          </button>
+          <button
+            className="btn-primary text-black"
+            style={{ backgroundColor: "Lavender" }}
+            onClick={() => setColor("lavender")}
+          >
+            Lavender
+          </button>
+          <button
+            className="btn-primary"
+            style={{ backgroundColor: "Black" }}
+            onClick={() => setColor("black")}
+          >
+            Black
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
